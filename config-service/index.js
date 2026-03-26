559aca52b7d21f4e7a65ecec25cd23521d1827ad
hridyen <hridyensharma@gmail.com>
2026-03-26 09:36:55 +0000

    config test

--- config-service/index.js
+++ config-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Config Service Running ⚙️');
+    res.send('Config Service Running');
 });
 
 app.listen(3002, () => {
