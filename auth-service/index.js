c32e15c3cd669f83bf9bdaeb553fa6b4a87dc345
hridyen <hridyensharma@gmail.com>
2026-03-26 09:34:24 +0000

    testing

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Auth Service Running testing ');
+    res.send('Auth Service Running fortest ');
 });
 
 app.listen(3001, () => {
