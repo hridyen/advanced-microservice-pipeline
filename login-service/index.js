9455a2349962fa289eb0200fae16d63fa1fdab26
hridyen <hridyensharma@gmail.com>
2026-03-26 09:52:05 +0000

    rr

--- login-service/index.js
+++ login-service/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Login Service Running 🔑');
+    res.send('Login Service Running');
 });
 
 app.listen(3003, () => {
